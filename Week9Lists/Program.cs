d77da9a978041ad6ae7d960ec3f87c2ea616bc8b
Harosshu <samuil.batsinski@gmail.com>
2024-04-28 20:31:57 +0100

    list

---
+++ Week9Lists/Program.cs
@@ -0,0 +1,56 @@
+﻿string folderPath = @"C:\data";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+List<string> myShoppingList = new List<string>();
+
+if (File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"File {fileName} has been created.");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+
+static List<string> GetItemsFromUser()
+{
+    List<string> userList = new List<string>();    
+
+    while (true)
+    {
+        Console.WriteLine("Add an item (add)/ Exit (exit):");
+        string userChoise = Console.ReadLine();
+
+        if (userChoise == "add")
+        {
+            Console.WriteLine("Enter an item:");
+            string userItem = Console.ReadLine();
+            userList.Add(userItem);
+        }
+        else
+        {
+            Console.WriteLine("Bye!");
+            break;
+        }
+
+    }
+    return userList;
+
+}
+
+Console.Clear();
+
+int listLenght  = myShoppingList.Count;
+Console.WriteLine($"You have added {listLenght} items to your shopping list.");
+
+int i = 1;
+foreach (string item in myShoppingList)
+{
+    Console.WriteLine($"{i}. {item}");
+    i++;
+}
